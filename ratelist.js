1bdf77f6dbe3cffb666a512e8a82eb39b053691c
pranavpandey02 <pranavpandey479@gmail.com>
2025-01-14 13:20:55 +0530

    commiting

--- ratelist.js
+++ ratelist.js
@@ -1,43 +1,43 @@
 // =================rate list data fetch===========
-// async function fet()
-// {
-//     let table1 = document.querySelector('#displaydata1');
-//     let res1 = await fetch("http://localhost:3000/emp");
-//     let data1 =await res1.json();
-//     console.log(data1);
+async function fet()
+{
+    let table1 = document.querySelector('#displaydata1');
+    let res1 = await fetch("http://localhost:3000/emp");
+    let data1 =await res1.json();
+    console.log(data1);
 
-//     let finaldata1 = data1.map((e)=>
-//     `
-//     <tr>
-//     <td>${e.id}</td>
-//     <td>${e.name}</td>
-//     <td>${e.mobilenum}</td>
-//     <td>${e.quantity}</td>
-//     <td>${e.pickupdate}</td>
-//     <td>${e.address}</td>
-//     <td>${e.product}</td>
-//     <td><button onclick="mydelete('${e.id}')"> Delete </button> </td>
+    let finaldata1 = data1.map((e)=>
+    `
+    <tr>
+    <td>${e.id}</td>
+    <td>${e.name}</td>
+    <td>${e.mobilenum}</td>
+    <td>${e.quantity}</td>
+    <td>${e.pickupdate}</td>
+    <td>${e.address}</td>
+    <td>${e.product}</td>
+    <td><button onclick="mydelete('${e.id}')"> Delete </button> </td>
 
-//     <td><button onclick="mydelete('${e.id}')"> Update </button> </td>
-//     </tr>
-//     `).join("")
-//     table1.innerHTML=finaldata1
+    <td><button onclick="mydelete('${e.id}')"> Update </button> </td>
+    </tr>
+    `).join("")
+    table1.innerHTML=finaldata1
 
-//     console.log(finaldata1);
+    console.log(finaldata1);
     
-// }
-// fet();
+}
+fet();
 
-// function mydelete(id)
-// {
-//     console.log(id);
-//         fet(`http://localhost:3000/emp${id}`,
-//             {
-//                 method:`DELETE`
-//             }
-//         )
-//     .then(r=>alert("Deleted......................!"))
-// }
+function mydelete(id)
+{
+    console.log(id);
+        fet(`http://localhost:3000/emp${id}`,
+            {
+                method:`DELETE`
+            }
+        )
+    .then(r=>alert("Deleted......................!"))
+}
 
 
 // ===================insterd data=================
@@ -52,7 +52,7 @@ function insert_data1()
         address:document.querySelector('#address').value,
         product:document.querySelector('#products').value
     }
-    fet("http://localhost:3000/emp",
+    fetch("http://localhost:3000/emp",
     {
         method:'POST',
        headers:
